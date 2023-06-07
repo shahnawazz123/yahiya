e05d194cc4ae9e84794d1875441cca4103e242e0
Shahnawaz <49223132+shahnawazz123@users.noreply.github.com>
2023-06-07 23:43:01 +0530

    Update m200101_000001_create_categories_table.php

--- migrations/m200101_000001_create_categories_table.php
+++ migrations/m200101_000001_create_categories_table.php
@@ -17,6 +17,20 @@ class m200101_000001_create_categories_table extends Migration
             'name' => $this->string()->notNull(),
             'description' => $this->text(),
         ]);
+        
+        $this->batchInsert('{{%categories}}', ['name', 'description'], [
+
+            ['Sneakers', 'Casual athletic shoes designed for comfort and versatility.'],
+
+            ['Boots', 'Footwear made of leather or synthetic materials, covering the foot and ankle or extending higher up the leg.'],
+
+            ['Sandals', 'Open-toe footwear with straps or a sole held to the foot using different fastening methods.'],
+
+            ['Flats', 'Low-heeled shoes that lack a significant arch, known for their comfort and ease of wearing.'],
+
+            ['High Heels', 'Women\'s shoes with raised heels of varying heights, commonly worn for formal events or to enhance a fashionable look.'],
+
+        ]);
     }
 
     /**
@@ -24,6 +38,7 @@ class m200101_000001_create_categories_table extends Migration
      */
     public function down()
     {
+        $this->delete('{{%categories}}');
         $this->dropTable('{{%categories}}');
     }
 }
